24416a753032c2904672e4a2404d456723fc96d3
Amrtaj <amrtaj100@gmail.com>
2025-07-20 13:19:19 +0300

    Add .next/static/chunks/src_app_apply_page_tsx_a993527f._.js 1753006730851

---
+++ .next/static/chunks/src_app_apply_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_8638a37a._.js",
+  "static/chunks/node_modules_6970461f._.js"
+],
+    source: "dynamic"
+});
